1dc27096e7b4cfd1b49065416a2551a850101ac4
Vincent Bernardoff <vb@luminar.eu.org>
2014-07-29 14:47:36 +0200

    Workingn distributed dict version

--- dict.ml
+++ dict.ml
@@ -42,9 +42,9 @@ struct
         Re.(exec ipv6addr s |> get_all)
       with
       | [|_;host;port;""|] ->
-        ADDR_INET (inet_addr_of_string host, int_of_string port), None
+        ADDR_INET (inet_addr_of_string host, int_of_string port)
       | [|_;host;port;zone|] ->
-        ADDR_INET (inet_addr_of_string host, int_of_string port), Some zone
+        ADDR_INET (inet_addr_of_string host, int_of_string port)
       | _ ->
         raise (Invalid_argument "invalid IPv6 address")
     else (* not an IPv6 address *)
@@ -52,10 +52,10 @@ struct
       let host, service = String.split ~by:":" s in
       match getaddrinfo host service [] with
       | [] -> raise (Invalid_argument "getaddrinfo returned no results")
-      | h::_ -> h.ai_addr, None
+      | h::_ -> h.ai_addr
     with Not_found ->
       Lwt_log.ign_warning_f ~section "Using UNIX domain socket %s" s;
-      ADDR_UNIX s, None
+      ADDR_UNIX s
 
   let node_sockaddr s = String.split ~by:"," s |> fst |> sockaddr
   let app_sockaddr  s =
@@ -169,6 +169,11 @@ let () =
   match !mode with
       `Help -> usage ()
     | `Master addr ->
+      let my_node_sockaddr = CONF.node_sockaddr addr in
+      let my_app_sockaddr = CONF.app_sockaddr addr in
+      let my_ports = match my_node_sockaddr, my_app_sockaddr with
+        | Unix.ADDR_INET (a, p),  Unix.ADDR_INET (a2, p2) -> p, p2
+        | _ -> failwith "my_node_port" in
       (match !mcast_addr with
        | None -> Lwt_main.run (run_server ~addr ?join:!cluster_addr ~id:addr ())
        | Some mcast_addr ->
@@ -177,35 +182,78 @@ let () =
          match Re.(exec ipv6addr mcast_addr |> get_all) with
          | [|_;v6addr;port;iface|] ->
            let port = int_of_string port in
+           let return_oraft_ports _ fd _ =
+             let buf = String.make 4 '\000' in
+             EndianString.BigEndian.set_int16 buf 0 (fst my_ports);
+             EndianString.BigEndian.set_int16 buf 2 (snd my_ports);
+             Lwt_unix.send fd buf 0 4 [] >>= fun nb_sent ->
+             assert (nb_sent = 4);
+             Lwt.return_unit
+           in
            let main_thread () =
-             Llnet.connect ~iface (Ipaddr.V6.of_string_exn v6addr) port >>= fun h ->
-             Llnet.neighbours h >>= function
-             | [] -> raise_lwt (Failure "invariant do not hold")
-             | sa::t ->
-               match sa with
-               | ADDR_UNIX _ -> raise_lwt (Failure "invariant do not hold")
-               | ADDR_INET (a, p) ->
-                 let cluster_addr =
-                   Printf.sprintf "[%s]:%d%%%s"
-                     (Unix.string_of_inet_addr a) p iface in
-                 run_server ~addr ?join:(Some cluster_addr) ~id:addr ()
+             Llnet.connect
+               ~tcp_reactor:return_oraft_ports
+               ~iface (Ipaddr.of_string_exn v6addr) port >>= fun h ->
+             (* Waiting for other peers to manifest themselves *)
+             Lwt_unix.sleep 2. >>= fun () ->
+             if Llnet.SaddrMap.cardinal h.peers = 1 then
+               (* We are alone, run server without joining a cluster *)
+               run_server ~addr ?join:!cluster_addr ~id:addr ()
+             else
+               Llnet.neighbours h >>= function
+               | [] -> raise_lwt (Failure "invariant do not hold")
+               | n ->
+                 let rec inner n =
+                   match n with
+                   | Unix.ADDR_UNIX _ -> raise_lwt (Failure "invariant do not hold")
+                   | Unix.ADDR_INET (a, p) ->
+                     (* Found one neighbour, asking his oraft node port *)
+                     let s_family = match Ipaddr_unix.of_inet_addr a with
+                       | V4 _ -> Unix.PF_INET
+                       | V6 _ -> Unix.PF_INET6 in
+                     let s = Lwt_unix.(socket s_family SOCK_STREAM 0) in
+                     Lwt_unix.connect s n >>= fun () ->
+                     let buf = String.make 4 '\000' in
+                     Lwt_unix.recv s buf 0 4 [] >>= fun nb_recv ->
+                     assert (nb_recv = 4);
+                     let remote_node_port = EndianString.BigEndian.get_int16 buf 0 in
+                     let remote_app_port = EndianString.BigEndian.get_int16 buf 2 in
+                     let uint16_of_int16 i16 = if i16 < 0 then i16 + 65535 else i16 in
+                     let remote_node_port = uint16_of_int16 remote_node_port in
+                     let remote_app_port = uint16_of_int16 remote_app_port in
+                     let cluster_addr =
+                       match s_family with
+                       | Unix.PF_UNIX -> assert false
+                       | Unix.PF_INET ->
+                         Printf.sprintf "%s:%d,%s:%d"
+                           (Unix.string_of_inet_addr a) remote_node_port
+                           (Unix.string_of_inet_addr a) remote_app_port
+                       | Unix.PF_INET6 ->
+                         Printf.sprintf "[%s]:%d,[%s]:%d"
+                           (Unix.string_of_inet_addr a) remote_node_port
+                           (Unix.string_of_inet_addr a) remote_app_port
+                     in
+                     Lwt_log.ign_notice_f "Connecting to peer at %s" cluster_addr;
+                     run_server ~addr ?join:(Some cluster_addr) ~id:addr ()
+                 in
+                 Lwt_list.iter_s inner n
            in Lwt_main.run (main_thread ())
          | _ -> failwith "Invalid multicast address: zone id missing"
 
       )
     | `Client addr ->
-        printf "Launching client %d\n" (Unix.getpid ());
-        if !ro_bm_iters > 0 then
-          Lwt_unix.run (ro_benchmark ~iterations:!ro_bm_iters ~addr ());
-        if !wr_bm_iters > 0 then
-          Lwt_unix.run (wr_benchmark ~iterations:!wr_bm_iters ~addr ());
+      printf "Launching client %d\n" (Unix.getpid ());
+      if !ro_bm_iters > 0 then
+        Lwt_unix.run (ro_benchmark ~iterations:!ro_bm_iters ~addr ());
+      if !wr_bm_iters > 0 then
+        Lwt_unix.run (wr_benchmark ~iterations:!wr_bm_iters ~addr ());
 
-        if !ro_bm_iters > 0 || !wr_bm_iters > 0 then exit 0;
+      if !ro_bm_iters > 0 || !wr_bm_iters > 0 then exit 0;
 
-        match !k, !v with
-            None, None | None, _ -> usage ()
-          | Some k, Some v ->
-              Lwt_unix.run (client_op ~addr (Set (k, v)))
-          | Some k, None ->
-              Lwt_unix.run (client_op ~addr (Wait k))
+      match !k, !v with
+        None, None | None, _ -> usage ()
+      | Some k, Some v ->
+        Lwt_unix.run (client_op ~addr (Set (k, v)))
+      | Some k, None ->
+        Lwt_unix.run (client_op ~addr (Wait k))
 
